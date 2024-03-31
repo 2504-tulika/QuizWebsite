18643a356257cb27677b6414ee012fa1f14153d8
2504-tulika <145529916+2504-tulika@users.noreply.github.com>
2024-03-31 21:14:56 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,27 @@
+
+# QUIZ WEBSITE
+
+Welcome to the Quiz Website repository! This project is a simple quiz application built using HTML, CSS, and JavaScript. It allows users to take quizzes on various topics and provides instant feedback on their answers.
+
+
+
+## Features
+- Multiple-choice questions
+- Instant feedback on answers
+- Score tracking
+- Responsive design for various screen sizes
+
+
+## Installation
+To run the Quiz Website locally, follow these steps:
+
+1. Clone this repository to your local machine.
+2. Navigate to the project directory using.
+3. Open the `index.html` file in your web browser.
+## Usage
+Once you have the website running, you can:
+
+- Select start quiz from the homepage.
+- Answer the questions by selecting the appropriate option.
+- Click the "Submit" button to see your score.
+- Optionally, retake the quiz.
---
+++ questions.js
@@ -0,0 +1,114 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What technology is used to record cryptocurrency transaction?",
+        answer: "C. Blockchain",
+        options: [
+            "A. Digital Wallet",
+            "B. Mining",
+            "C. Blockchain",
+            "D. Token"
+
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does acronym FOSS stands for?",
+        answer: "A. Free and Open Source Software",
+        options: [
+            "A. Free and Open Source Software",
+            "B. Full Option Sensor System",
+            "C. Follow On Support Service",
+            "D. Fiber Optics Science System"
+
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which tech buzzword is closely related to AI?",
+        answer: "A. Virtual Reality",
+        options: [
+            "A. Virtual Reality",
+            "B. Machine Learing",
+            "C. Cryptocurrency",
+            "D. Microservices"
+        ]
+    },
+    {
+        numb: 4,
+        question: "First cross platform pdf Software-",
+        answer: "D. Able2Extract Professional",
+        options: [
+            "A. Adobe Acrobat",
+            "B. Foxit Platform",
+            "C. Nitro Pro",
+            "D. Able2Extract Professional"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What year was the oldest programming language still in use invented?",
+        answer: "A. 1957",
+        options: [
+            "A. 1957",
+            "B. 1952",
+            "C. 1960",
+            "D. 1974"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Who is the founder of Oracle?",
+        answer: "B. Larry Ellison",
+        options: [
+            "A. Christ Lamb",
+            "B. Larry Ellison",
+            "C. James Carter",
+            "D. Henry Smith"
+        ]
+    },
+    {
+        numb: 7,
+        question: "What does HP stand for?",
+        answer: "C. Hewlett-Packard",
+        options: [
+            "A. Hewiit-Packard",
+            "B. Henry-Packer",
+            "C. Hewlett-Packard",
+            "D. Helson-Packson"
+        ]
+    },
+    {
+        numb: 8,
+        question: "LinkedIn Partnered with which organisation to provide digital skills training for youth?",
+        answer: "B. NSDC",
+        options: [
+            "A. CBSE",
+            "B. NSDC",
+            "C. E-skill",
+            "D. None of these"
+        ]
+    },
+    {
+        numb: 9,
+        question: "What is the opposite of GIT clone?",
+        answer: "A. GIT push",
+        options: [
+            "A. GIT push",
+            "B. GIT upload",
+            "C. GIT status",
+            "D. GIT add"
+        ]
+    },
+    {
+        numb: 10,
+        question: "In JavaScript, 'this' refers to the object that _______ the object.",
+        answer: "D. Owns",
+        options: [
+            "A. Receives",
+            "B. Depends",
+            "C. Direct",
+            "D. Owns"
+        ]
+    },
+];
